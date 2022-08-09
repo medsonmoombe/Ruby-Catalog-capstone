326cfab27762f830636861984c7c814051e9a1a6
medsonmoombe <medsonmoombe21@gmail.com>
2022-08-09 23:43:55 +0200

    Add the main as the entry point and add the options to be displayed to the user

---
+++ utils/options.rb
@@ -0,0 +1,53 @@
+require './classes/item'
+
+# rubocop:disable Metrics/CyclomaticComplexity
+class Options
+  def show_menu
+    puts 'Enter a number to choose an option below: '
+    puts '1. List All Books'
+    puts '2. List All Labels'
+    puts '3. List All Music Albums'
+    puts '4. List All Genres'
+    puts '5. List Of Games'
+    puts '6. List All Authors'
+    puts '7. Add A Book'
+    puts '8. Add a Music Album'
+    puts '9. Add A Genre'
+    puts '10. Add A Game'
+    puts '11. Add An Author'
+    puts '12. Add Label'
+    puts '13. Quit'
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def menu_options
+    case option
+    when '1'
+      #   list_book
+    when '2'
+      #   list_label
+    when '3'
+      #   all_music_albums
+    when '4'
+    #   all_genres
+    when '5'
+    #   list_game
+    when '6'
+    #   all_authors
+    when '7'
+    #   add_book
+    when '8'
+    #   create_music_album
+    when '9'
+    #   create_genre
+    when '10'
+    #   add_game
+    when '11'
+    #   create_author
+    when '12'
+      # add_label
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
+end
+# rubocop:enable Metrics/CyclomaticComplexity
